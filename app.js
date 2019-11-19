59f06ddf9d28832f2c24cbadb5e10a750b6ffbfe
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-19 11:29:03 -0600

    created and rendered nested React components GroceryList

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var GroceryList = () => (
+  <ul>
+    <li>Milk</li>
+    <li>Salad</li>
+    <li>Watermelon</li>
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My GroceryList</h2>
+    <GroceryList />
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
