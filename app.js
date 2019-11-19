42519127a6993ac7c24a51af794ce4bd54efce67
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-19 12:53:31 -0600

    created React components for the 2 items in the grocery list

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // TODO
-var GroceryList = () => (
-  <ul>
-    <li>Milk</li>
-    <li>Salad</li>
-    <li>Watermelon</li>
-  </ul>
-);
-
 var App = () => (
   <div>
     <h2>My GroceryList</h2>
     <GroceryList />
   </div>
+);
+
+var GroceryList = () => (
+  <ul>
+    <Cucumbers />
+    <Kale />
+
+  </ul>
+);
+
+var Cucumbers = () => (
+  <li>Cucumbers</li>
+);
+
+var Kale = () => (
+  <li>Kale</li>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"))
