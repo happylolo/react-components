a5be62e4b9915281a8cada18c2936a943fcba420
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-19 14:52:29 -0600

    Refactor GroceryListItem as a class component

--- app.js
+++ app.js
@@ -6,16 +6,32 @@ var App = () => (
 );
 
 var GroceryList = () => (
-  <GroceryListItem items = {['Cucumbers', 'Kale', 'Butter']} />
+  <GroceryListItem items = {['Cucumbers', 'Kyle', 'Butter']} />
 );
 
-var GroceryListItem = (props) => (
-  // passing properties from Parent to Child, using props
-  <ul>
-    <li>{props.items[0]}</li>
-    <li>{props.items[1]}</li>
-    <li>{props.items[2]}</li>
-  </ul>
-);
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
+class GroceryListItem extends React.Component {
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    super(props);
+  }
+
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+  render() {
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+      <ul>
+        <li>{this.props.items[0]}</li>
+        <li>{this.props.items[1]}</li>
+        <li>{this.props.items[2]}</li>
+      </ul>
+    );
+  };
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
